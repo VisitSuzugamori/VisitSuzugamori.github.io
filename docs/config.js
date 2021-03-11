10da8e567f9606dcbe8804b33dc0c56e437b55f2
ISHIDA Naoto <naoto@isnot.jp>
2021-03-11 18:19:11 +0900

    tempolary add demo.html

---
+++ docs/config.js
@@ -0,0 +1,165 @@
+const config = {
+  style: 'mapbox://styles/isnot/ckdzfj1n70ee219lhgygiamwc',
+  accessToken: 'pk.eyJ1IjoiaXNub3QiLCJhIjoiY2tscDdpeGExMHp5cTJwcXI0YWlzZHdyeCJ9._NooEseBc8wEwa3z_plcAg',
+  showMarkers: true,
+  markerColor: '#3FB1CE',
+  theme: 'light',
+  use3dTerrain: true,
+  title: 'VisitSuzugamori 07',
+  subtitle: '【07】',
+  byline: '地図作成 isnot',
+  footer: 'ざつ旅-That&#39;s Journey- (c) 石坂 ケンタ, KADOKAWA, 電撃マオウ / Mapbox',
+  chapters: [
+    {
+      id: '2巻-P73-粟島観光案内所',
+      alignment: 'left',
+      hidden: false,
+      title: '粟島観光案内所',
+      image: '',
+      description: '2巻 P73 ',
+      location: {
+        center: [139.2549057, 38.4674427],
+        zoom: 13,
+        pitch: 60,
+        bearing: 0,
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      callback: '',
+      onChapterEnter: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 1,
+        //     duration: 5000
+        // }
+      ],
+      onChapterExit: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 0
+        // }
+      ],
+    },
+    {
+      id: '2巻-P75-源左エ門',
+      alignment: 'left',
+      hidden: false,
+      title: '源左エ門',
+      image: '',
+      description: '2巻 P75 ',
+      location: {
+        center: [139.2231251, 38.4469905],
+        zoom: 13,
+        pitch: 60,
+        bearing: 0,
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      callback: '',
+      onChapterEnter: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 1,
+        //     duration: 5000
+        // }
+      ],
+      onChapterExit: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 0
+        // }
+      ],
+    },
+    {
+      id: '2巻-P78-矢ヶ鼻展望台',
+      alignment: 'left',
+      hidden: false,
+      title: '矢ヶ鼻展望台',
+      image: '',
+      description: '2巻 P78 ',
+      location: {
+        center: [139.2239814, 38.4374624],
+        zoom: 13,
+        pitch: 60,
+        bearing: 0,
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      callback: '',
+      onChapterEnter: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 1,
+        //     duration: 5000
+        // }
+      ],
+      onChapterExit: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 0
+        // }
+      ],
+    },
+    {
+      id: '2巻-P80-みやこや食堂',
+      alignment: 'left',
+      hidden: false,
+      title: 'みやこや食堂',
+      image: '',
+      description: '2巻 P80 ',
+      location: {
+        center: [139.2530973, 38.4650772],
+        zoom: 13,
+        pitch: 60,
+        bearing: 0,
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      callback: '',
+      onChapterEnter: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 1,
+        //     duration: 5000
+        // }
+      ],
+      onChapterExit: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 0
+        // }
+      ],
+    },
+    {
+      id: '2巻-P82-漁火温泉 おと姫の湯',
+      alignment: 'left',
+      hidden: false,
+      title: '漁火温泉 おと姫の湯',
+      image: '',
+      description: '2巻 P82 ',
+      location: {
+        center: [139.2549858, 38.4687521],
+        zoom: 13,
+        pitch: 60,
+        bearing: 0,
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      callback: '',
+      onChapterEnter: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 1,
+        //     duration: 5000
+        // }
+      ],
+      onChapterExit: [
+        // {
+        //     layer: 'layer-name',
+        //     opacity: 0
+        // }
+      ],
+    },
+
+  ],
+};
