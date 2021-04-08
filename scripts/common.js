f0436a0e39f39ab684eac55cff9bf1aa351f1c35
ISHIDA Naoto <naoto@isnot.jp>
2021-04-08 18:46:34 +0900

    add common.js. for some functions

---
+++ scripts/common.js
@@ -0,0 +1,65 @@
+class Common {
+  simple_wait_sec(sec = 1) {
+    return new Promise((resolve) => setTimeout(resolve, sec * 1000));
+  }
+
+  safeRetrieve(target = {}, key = '', alternate = '') {
+    if (typeof target !== 'object' || target === null) {
+      return alternate;
+    }
+    try {
+      const myMap = new Map(Object.entries(target));
+      if (myMap.has(key)) {
+        const value = myMap.get(key);
+        myMap.clear();
+        return typeof value === typeof alternate ? value : alternate;
+      }
+      myMap.clear();
+      return alternate;
+    } catch (e) {
+      return alternate;
+    }
+  }
+
+  deepRetrieve(target, pos, alternate = undefined) {
+    let cur = target;
+    if (typeof pos !== 'string' || typeof target !== 'object' || target === null) {
+      return alternate;
+    }
+    try {
+      const pos_keys = pos.split('.');
+      const pos_len = pos_keys.length;
+      pos_keys.forEach((el, idx) => {
+        const wants = idx === pos_len - 1 ? alternate : {};
+        cur = this.safeRetrieve(cur, el, wants);
+      });
+    } catch (e) {
+      return alternate;
+    }
+    return cur;
+  }
+
+  hasProperty(obj, prop) {
+    // const hasProperty = Object.prototype.hasOwnProperty;
+    // return Object.prototype.hasOwnProperty.call(obj, prop);
+    return new Map(Object.entries(obj)).has(prop);
+  }
+
+  replaceCharactorEntity4Html(text) {
+    if (typeof text !== 'string' || !text) {
+      return text;
+    }
+    const ent = {
+      '<': '&lt;',
+      '>': '&gt;',
+      '&': '&amp;',
+      // '*' : '&#42;',
+      // '_' : '&#95;',
+    };
+    return text.replace(/[<>&]/g, (match) => {
+      return ent[`${match}`];
+    });
+  }
+}
+
+module.exports = new Common();
