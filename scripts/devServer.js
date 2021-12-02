74e38faba473d45910eda3926c6d33055823c4d6
ISHIDA Naoto <naoto@isnot.jp>
2021-12-03 00:01:03 +0900

    add local dev server via https

---
+++ scripts/devServer.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const https = require('https');
+const connect = require('connect');
+const logger = require('morgan');
+const serveStatic = require('serve-static');
+
+const options = {
+  cert: fs.readFileSync('./cert/server.crt'),
+  key: fs.readFileSync('./cert/server.key'),
+};
+
+const app = connect().use(logger('dev')).use(serveStatic('../docs/'));
+
+https.createServer(options, app).listen(3000);
+console.log('start serv');
